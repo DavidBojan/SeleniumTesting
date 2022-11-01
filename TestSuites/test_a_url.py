6981b6f50c8bd3d6956d36afe3e010b72e1af4d8
David Bojan <bojandavid98@gmail.com>
2022-11-01 21:32:25 +0200

    TestSuites

--- TestSuites/test_a_url.py
+++ TestSuites/test_a_url.py
@@ -1,6 +1,4 @@
 def test_url(set_up):
     actual = set_up.current_url
     expected = 'https://the-internet.herokuapp.com/login'
-    # expected value, actual value, mesaj in caz de fail
-    # set_up.assertEqual(expected, actual, 'URL is incorrect')
     assert expected == actual, f'Expected: {expected} . Actual: {actual}'
--- TestSuites/test_g_error_message_text.py
+++ TestSuites/test_g_error_message_text.py
@@ -7,8 +7,8 @@ expected_data = ExpectedData()
 
 
 def test_error_message_text(set_up):
-    set_up.find_element(By.ID, selectors_data.username_id).send_keys(expected_data.username)
-    set_up.find_element(By.ID, selectors_data.password_id).send_keys(expected_data.password)
+    set_up.find_element(By.ID, selectors_data.username_id).send_keys(expected_data.wrong_username)
+    set_up.find_element(By.ID, selectors_data.password_id).send_keys(expected_data.wrong_password)
     set_up.find_element(By.CSS_SELECTOR, selectors_data.login_button).click()
     text_displayed = set_up.find_element(By.CSS_SELECTOR, selectors_data.error_displayed).text
     expected = expected_data.text_expected
